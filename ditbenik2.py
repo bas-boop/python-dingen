f65e50bfa07516fc03a6b90e35d1356e07f38c76
Bas <30810@ma-web.nl>
2020-10-07 12:38:51 +0200

    ditbenik2 is nu af.

--- ditbenik2.py
+++ ditbenik2.py
@@ -15,7 +15,6 @@ def programma():
     vraag1 = "1. Welke opleiding heb ik gekeuzen?\nA. Media developer\nB. software developer\nC. game developer"
     vraag2 = "2. Welke kleur haar heb ik?\nA. groen\nB. roze\nC. roze met wat zilver"
     vraag3 = "3. Waar heb ik op school gezeten?\nA. basicschool\nB. middelbareschool\nC. college"
-    vraag4 = "4. hier komt een vraag.\nA.\nB.\nC."
 
     print("")
     print(vraag1)
@@ -50,26 +49,40 @@ def programma():
 
     print(vraag3)
 
+    def vraag4():
+
+        print("4. Wat is mijn favorieten eten?\nA. Macaroni\nB. Shusi\nC. kiptandori")
+        antwoordvraag4 = input()
+        if antwoordvraag4 == "a" or antwoordvraag4 == "A" or antwoordvraag4 == "c" or antwoordvraag4 == "C":
+            print("Het is wel lekker maar niet het lekkers.")
+        elif antwoordvraag4 == "b" or antwoordvraag4 == "B":
+            print("Shusi is natuurlijk mijn favo eten.")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag4()
+
+    def vraag5():
+
+        print("5. Wat is mijn hobby?\nA. Gamen\nB. Paardrijden\nC. Fitness")
+        antwoordvraag4 = input()
+        if antwoordvraag4 == "a" or antwoordvraag4 == "A" or antwoordvraag4 == "c" or antwoordvraag4 == "C":
+            print("Ja dat klopt.")
+            print("Je kent me nu wel goed :D")
+        elif antwoordvraag4 == "b" or antwoordvraag4 == "B":
+            print("Nee ik heb nooit op een paard gerijden.")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag5()
+
     def vraag3Juist():
 
         antwoordVraag3 = input()
         if antwoordVraag3 == "a" or antwoordVraag3 == "b" or antwoordVraag3 == "A" or antwoordVraag3 == "B":
-            print("Ja, maar nu niet meer.")
+            print("Ja, maar nu niet meer.\nJe kent mij niet goed, jij moet meer vragen beantworden.\n")
+            print(vraag5())
         elif antwoordVraag3 == "c" or antwoordVraag3 == "C":
-            print("Ja, ik zit daar nu op.\nJe kent mij best goed, jij mag meer vragen beantworden.")
-            print(vraag4)
-
-            def vraag4juist():
-                
-                antwoordvraag4 = input()
-                if antwoordvraag4 == "a" or antwoordvraag4 == "A" or antwoordvraag4 == "c" or antwoordvraag4 == "C":
-                    print("a of c")
-                elif antwoordvraag4 == "b" or antwoordvraag4 == "B":
-                    print("b")
-                else:
-                    print("Je mag alleen a, b of c typen.")
-                    vraag4Juist()
-            vraag4Juist()
+            print("Ja, ik zit daar nu op.\nJe kent mij best goed, jij mag meer vragen beantworden.\n")
+            print(vraag4())
 
         else:
             print("Je mag alleen a, b of c typen.")
@@ -81,6 +94,6 @@ def programma():
     if herstarten == "Y" or herstarten == "y":
         programma()
     else:
-        print("Tot ziens " + naam)
+        print("Bye bye " + naam)
 
 programma()
