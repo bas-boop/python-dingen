67e6f84bab58717648c1744962fcb28cfb3afb2c
Bas <30810@ma-web.nl>
2020-10-20 16:25:19 +0200

    verhaal toegevoegd

---
+++ verhaal.py
@@ -0,0 +1,283 @@
+def programma():
+    print("\nWelke bij het verhaal over een nieuwkomer.")
+    print("Wat is jou naam? (met hoofdletter graag)")
+    naam = input()
+
+    begin_stuk = "Je bent een man die vanaf Pakistan naar Nederland wilt vluchten."
+
+    def vraag21():
+
+        antwoord21 = input()
+        if antwoord21 == "a" or antwoord21 == "A":
+            print("reeee1")
+        elif antwoord21 == "b" or antwoord21 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag21()
+#--------------------------------------------------------------------------------------------------------------------------------
+    def vraag20():
+
+        antwoord20 = input()
+        if antwoord20 == "a" or antwoord20 == "A":
+            print("reeee1")
+        elif antwoord20 == "b" or antwoord20 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag20()
+
+    def vraag19():
+
+        antwoord19 = input()
+        if antwoord19 == "a" or antwoord19 == "A":
+            print("reeee1")
+        elif antwoord19 == "b" or antwoord19 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag19()
+
+    def vraag18():
+
+        antwoord18 = input()
+        if antwoord18 == "a" or antwoord18 == "A":
+            print("reeee1")
+        elif antwoord18 == "b" or antwoord18 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag18()
+
+    def vraag17():
+
+        antwoord17 = input()
+        if antwoord17 == "a" or antwoord17 == "A":
+            print("reeee1")
+        elif antwoord17 == "b" or antwoord17 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag17()
+
+    def vraag16():
+
+        antwoord16 = input()
+        if antwoord16 == "a" or antwoord16 == "A":
+            print("reeee1")
+        elif antwoord16 == "b" or antwoord16 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag16()
+#--------------------------------------------------------------------------------------------------------------------------------
+    def vraag15():
+
+        antwoord15 = input()
+        if antwoord15 == "a" or antwoord15 == "A":
+            print("reeee1")
+        elif antwoord15 == "b" or antwoord15 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag15()
+
+    def vraag14():
+
+        antwoord14 = input()
+        if antwoord14 == "a" or antwoord14 == "A":
+            print("reeee1")
+        elif antwoord14 == "b" or antwoord14 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag14()
+
+    def vraag13():
+
+        antwoord13 = input()
+        if antwoord13 == "a" or antwoord13 == "A":
+            print("reeee1")
+        elif antwoord13 == "b" or antwoord13 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag13()
+
+    def vraag12():
+
+        antwoord12 = input()
+        if antwoord12 == "a" or antwoord12 == "A":
+            print("reeee1")
+        elif antwoord12 == "b" or antwoord12 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag12()
+
+    def vraag11():
+
+        antwoord11 = input()
+        if antwoord11 == "a" or antwoord11 == "A":
+            print("reeee1")
+        elif antwoord11 == "b" or antwoord11 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag11()
+#--------------------------------------------------------------------------------------------------------------------------------
+    def vraag10():
+
+        antwoord10 = input()
+        if antwoord10 == "a" or antwoord10 == "A":
+            print("reeee1")
+        elif antwoord10 == "b" or antwoord10 == "B":
+            print("reeee2")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag10()
+
+    def vraag9():
+
+        antwoord9 = input()
+        if antwoord9 == "a" or antwoord9 == "A":
+            print("ja")
+            print(vraag10())
+        elif antwoord9 == "b" or antwoord9 == "B":
+            print("nee")
+            print("(normaal einde)")
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag9()
+
+    def vraag8():
+
+        antwoord8 = input()
+        if antwoord8 == "a" or antwoord8 == "A":
+            print("lopen")
+            print("\nJe liep verder en je had geen eten en drinken meer toen ging je dood. (dood einde)")
+        elif antwoord8 == "b" or antwoord8 == "B":
+            print("fiets")
+            print(vraag3())
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag8()
+
+    def vraag7():
+
+        antwoord7 = input()
+        if antwoord7 == "a" or antwoord7 == "A":
+            print("nee")
+            print("(normaal einde)")
+        elif antwoord7 == "b" or antwoord7 == "B":
+            print("ja")
+            print(vraag4())
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag7()
+
+    def vraag6():
+
+        antwoord6 = input()
+        if antwoord6 == "a" or antwoord6 == "A":
+            print("ja")
+            print(vraag7())
+        elif antwoord6 == "b" or antwoord6 == "B":
+            print("nee")
+            print(vraag8())
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag6()
+
+#--------------------------------------------------------------------------------------------------------------------------------
+
+    def vraag5():
+
+        antwoord5 = input()
+        if antwoord5 == "a" or antwoord5 == "A":
+            print("wegen")
+            print(vraag6())
+        elif antwoord5 == "b" or antwoord5 == "B":
+            print("woestijn")
+            print("\nJe liep door het woestijn heen en je had geen eten en drinken meer toen ging je dood. (dood einde)")
+        elif antwoord5 == "c" or antwoord5 == "C":
+            print("waterkant")
+            print(vraag3())
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag5()
+
+    def vraag4():
+
+        antwoord4 = input()
+        if antwoord4 == "a" or antwoord4 == "A":
+            print("nee")
+            print("Omdat je de kroket niet wpu proberen ben je weg gestuurd uit Nederland en ben je heel erg verward geraakt. Je was zo erg verward dat je weer terug ging naar Pakistan. (slecht einde)")
+        elif antwoord4 == "b" or antwoord4 == "B":
+            print("ja")
+            print(vraag13())
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag4()
+
+    def vraag3():
+
+        antwoord3 = input()
+        if antwoord3 == "a" or antwoord3 == "A":
+            print("ja")
+            print(vraag4())
+        elif antwoord3 == "b" or antwoord3 == "B":
+            print("nee")
+            print(vraag9())
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag3()
+
+    def vraag2():
+
+        print("vraag 2")
+
+        antwoord2 = input()
+        if antwoord2 == "a" or antwoord2 == "A":
+            print("auto")
+            print(vraag3())
+        elif antwoord2 == "b" or antwoord2 == "B":
+            print("vliegtuig")
+            print(vraag7())
+        elif antwoord2 == "c" or antwoord2 == "C":
+            print("lopend")
+            print(vraag5())
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag2()
+
+    def vraag1():
+
+        print("vraag 1\n1. eerste keuzen\n2. tweede keuze\n3. derede keuze")
+
+        antwoord1 = input()
+        if antwoord1 == "a" or antwoord1 == "A":
+            print("a")
+            print(vraag2())
+        elif antwoord1 == "b" or antwoord1 == "B":
+            print("b")
+            print(vraag16())
+        elif antwoord1 == "c" or antwoord1 == "C":
+            print("Je moeder vond het geen goed idee en heeft je geslagen met een slipper. (slecht einde, want je bent niet gevlucht.)")
+            return
+        else:
+            print("Je mag alleen a, b of c typen.")
+            vraag1()
+        vraag1()
+#--------------------------------------------------------------------------------------------------------------------------------
+    print("\n")
+    print(vraag1())
+
+    print("\nwil je het herstarten? Y/N")
+    herstarten = input()
+    if herstarten == "Y" or herstarten == "y":
+        programma()
+    else:
+        print("Bye bye " + naam)
+
+programma()
